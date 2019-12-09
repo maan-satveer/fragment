948c24759af131e575829d50443afa42d3664c94
macstudent <chahal.rajdeep03@gmail.com>
2019-12-09 16:30:13 -0500

    fragment create

---
+++ app/src/main/java/com/example/fragmentdemo/PositionTitleFragment.java
@@ -0,0 +1,33 @@
+package com.example.fragmentdemo;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PositionTitleFragment extends ListFragment {
+
+
+    public PositionTitleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
