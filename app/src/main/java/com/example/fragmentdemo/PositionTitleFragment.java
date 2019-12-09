5ed9a1619ec5ba995b291b94a98f62c6b1ab2e78
macstudent <chahal.rajdeep03@gmail.com>
2019-12-09 16:51:20 -0500

    list fragment

--- app/src/main/java/com/example/fragmentdemo/PositionTitleFragment.java
+++ app/src/main/java/com/example/fragmentdemo/PositionTitleFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 /**
@@ -26,7 +27,12 @@ public class PositionTitleFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
+        String[] titles = new String[Position.positions.length];
+        for (int i=0; i<titles.length; i++){
+            titles[i] = Position.positions[i].getTitle();
+            ArrayAdapter adapter = new ArrayAdapter(inflater.getContext(),android.R.layout.simple_list_item_1,titles);
+            setListAdapter(adapter);
+        }
         return super.onCreateView(inflater,container,savedInstanceState);
     }
 
